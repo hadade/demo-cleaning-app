7b1a2ce30976475fa3f71236e6c1de2cf0754667
hadade <hadadesergiuandrei@gmail.com>
2025-08-28 18:25:55 +0200

    add workflow

--- next.config.js
+++ next.config.js
@@ -14,7 +14,8 @@ const nextConfig = {
     ],
   },
   basePath: process.env.NODE_ENV === 'production' ? '/demo-cleaning-app' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/demo-cleaning-app/' : ''
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/demo-cleaning-app/' : '',
+  trailingSlash: true, // optional, avoids GitHub Pages 404s on nested routes
 };
 
 module.exports = nextConfig;
